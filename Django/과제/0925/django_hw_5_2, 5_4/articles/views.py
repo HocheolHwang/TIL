33b9492942462567c71b278d26480ac8f40e28a1
HocheolHwang <inpofes7849@naver.com>
2023-09-26 12:14:36 +0900

    0926

--- Django/과제/0925/django_hw_5_2, 5_4/articles/views.py
+++ Django/과제/0925/django_hw_5_2, 5_4/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
 
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -21,4 +22,18 @@ def create(request):
     article = Article(title=title, content=content)
     article.save()
 
+    return redirect('articles:index')
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
     return redirect('articles:index')
